447c310da6f2188cbffbd7e1cd08ef9eff7dadb8
James <james@jarofgreen.co.uk>
2011-09-02 20:00:38 +0100

    marker on map

--- page1.js
+++ page1.js
@@ -1,4 +1,9 @@
-var map, layer;
+var map, layer, markerLayer, marker;
+
+var size = new OpenLayers.Size(21,25);
+var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png',size,offset);
+
 
 $(document).ready(function() {
 
@@ -6,11 +11,15 @@ $(document).ready(function() {
 	layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
 	map.addLayer(layer);
 	map.setCenter(
-		new OpenLayers.LonLat(-71.147, 42.472).transform(
+		new OpenLayers.LonLat(geoip_longitude(), geoip_latitude()).transform(
 			new OpenLayers.Projection("EPSG:4326"),
 			map.getProjectionObject()
 		), 4
 	);
+
+	markerLayer = new OpenLayers.Layer.Markers( "Markers" );
+	map.addLayer(markerLayer);
+
 
 
 	setLocation(geoip_latitude(),geoip_longitude());
@@ -20,6 +29,11 @@ $(document).ready(function() {
 function setLocation(lat,lng) {
 	$('#LatVal').val(lat);
 	$('#LngVal').val(lng);
+
+	var pos = new OpenLayers.LonLat(lng,lat).transform(new OpenLayers.Projection("EPSG:4326"),map.getProjectionObject());
+	marker = new OpenLayers.Marker(pos,icon);
+	markerLayer.addMarker(marker);
+
 }
 
 
