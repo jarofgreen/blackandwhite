884d1738b118088550c90eebef0ff8667e86cfce
James <james@jarofgreen.co.uk>
2011-09-02 19:48:51 +0100

    show OSM

---
+++ page1.js
@@ -0,0 +1,19 @@
+var map, layer;
+
+$(document).ready(function() {
+
+	map = new OpenLayers.Map( 'Map');
+	layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
+	map.addLayer(layer);
+	map.setCenter(
+		new OpenLayers.LonLat(-71.147, 42.472).transform(
+			new OpenLayers.Projection("EPSG:4326"),
+			map.getProjectionObject()
+		), 4
+	);
+
+
+});
+
+
+
