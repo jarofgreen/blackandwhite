070f46a45df12b4dce648252171e9d87977d830d
James <james@jarofgreen.co.uk>
2011-09-02 20:53:01 +0100

    click to set location

--- page1.js
+++ page1.js
@@ -20,15 +20,51 @@ $(document).ready(function() {
 	markerLayer = new OpenLayers.Layer.Markers( "Markers" );
 	map.addLayer(markerLayer);
 
-
-
 	setLocation(geoip_latitude(),geoip_longitude());
+
+	OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
+		defaultHandlerOptions: {
+			'single': true,
+			'double': false,
+			'pixelTolerance': 0,
+			'stopSingle': false,
+			'stopDouble': false
+		},
+
+		initialize: function(options) {
+			this.handlerOptions = OpenLayers.Util.extend(
+				{}, this.defaultHandlerOptions
+			);
+			OpenLayers.Control.prototype.initialize.apply(
+				this, arguments
+			);
+			this.handler = new OpenLayers.Handler.Click(
+				this, {
+					'click': this.trigger
+				}, this.handlerOptions
+			);
+		},
+
+		trigger: function(e) {
+			var lonlat = map.getLonLatFromViewPortPx(e.xy).transform(map.getProjectionObject(),new OpenLayers.Projection("EPSG:4326"));
+			setLocation(lonlat.lat,lonlat.lon);
+		}
+
+	});
+
+	var click = new OpenLayers.Control.Click();
+	map.addControl(click);
+	click.activate();
 
 });
 
 function setLocation(lat,lng) {
 	$('#LatVal').val(lat);
 	$('#LngVal').val(lng);
+
+	if (marker) {
+		markerLayer.removeMarker(marker);
+	}
 
 	var pos = new OpenLayers.LonLat(lng,lat).transform(new OpenLayers.Projection("EPSG:4326"),map.getProjectionObject());
 	marker = new OpenLayers.Marker(pos,icon);
