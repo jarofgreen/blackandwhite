6418e781233faee6e1494bd501bbcf63117becec
James <james@jarofgreen.co.uk>
2011-09-09 08:35:01 +0100

    don't add more that 1 marker on the same location - to many markers slows the map down

--- page2.js
+++ page2.js
@@ -17,12 +17,18 @@ $(document).ready(function() {
 	markerLayer = new OpenLayers.Layer.Markers( "Markers" );
 	map.addLayer(markerLayer);
 
+	var markersAdded = new Object;
+
 	for(i in data) {
-		var pos = new OpenLayers.LonLat(data[i].lng,data[i].lat).transform(new OpenLayers.Projection("EPSG:4326"),map.getProjectionObject());
-		var marker = new OpenLayers.Marker(pos,icon.clone());
-		eval("f = function(evt) { markerClicked("+data[i].lat+","+data[i].lng+");  OpenLayers.Event.stop(evt); }");
-		marker.events.register('mousedown', marker, f);
-		markerLayer.addMarker(marker);
+		var hashString = "LT"+roundLatLng(data[i].lat)+"LG"+roundLatLng(data[i].lng);
+		if (!markersAdded[hashString]) {
+			var pos = new OpenLayers.LonLat(data[i].lng,data[i].lat).transform(new OpenLayers.Projection("EPSG:4326"),map.getProjectionObject());
+			var marker = new OpenLayers.Marker(pos,icon.clone());
+			eval("f = function(evt) { markerClicked("+data[i].lat+","+data[i].lng+");  OpenLayers.Event.stop(evt); }");
+			marker.events.register('mousedown', marker, f);
+			markerLayer.addMarker(marker);
+			markersAdded[hashString] = true;
+		}
 	}
 
 	map.zoomToExtent(markerLayer.getDataExtent());
